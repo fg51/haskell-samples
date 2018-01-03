5441972e62b016db75aa975bf50c7cbb1cf2afbd
kflange <zknsftk1msrk@gmail.com>
2018-01-04 02:37:45 +0900

    feature: add parser

--- cli/src/Data/Hjq/Parser.hs
+++ cli/src/Data/Hjq/Parser.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Hjq.Parser (JqFilter(..), parseJqFilter) where
 
--- import Control.Applicative ((<*))
-import Data.Attoparsec.Text (parse, feed, Result, endOfInput, Parser)
-import Data.Text (Text)
+import Control.Applicative ((<|>))
+import Data.Attoparsec.Text (parse, feed, Result, endOfInput, Parser, IResult(..), char, skipSpace, decimal, many1, letter, digit)
+import Data.Text (Text, pack)
 
 data JqFilter
   = JqField Text JqFilter
@@ -16,8 +16,23 @@ parseJqFilter s = showParseResult $ parse (jqFilterParser <* endOfInput) s `feed
 
 
 jqFilterParser :: Parser JqFilter
-jqFilterParser = undefined
+jqFilterParser = schar '.' >> (jqField <|> jqIndex <|> pure JqNil)
+  where
+    jqFilter :: Parser JqFilter
+    jqFilter = (schar '.' >> jqField) <|> jqIndex <|> pure JqNil
 
+    jqField :: Parser JqFilter
+    jqField = JqField <$> (word <* skipSpace) <*> jqFilter
+
+    jqIndex :: Parser JqFilter
+    jqIndex = JqIndex <$> (schar '[' *> decimal <* schar ']') <*> jqFilter
 
 showParseResult :: Show a => Result a -> Either Text a
-showParseResult = undefined
+showParseResult (Done _ r) = Right r
+showParseResult r = Left . pack $ show r
+
+schar :: Char -> Parser Char
+schar c = skipSpace *> char c <* skipSpace
+
+word :: Parser Text
+word = fmap pack $ many1 (letter <|> char '-' <|> char '_' <|> digit)
